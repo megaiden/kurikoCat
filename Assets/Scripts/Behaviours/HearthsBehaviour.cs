b26d462cb81ca575368a233c0070caa9fa59ac05
mizhaer <mishael.ochoa@illogika.com>
2023-02-10 17:02:48 -0500

    hearths first push

---
+++ Assets/Scripts/Behaviours/HearthsBehaviour.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Behaviours
+{
+    public  class  HearthsBehaviour : MonoBehaviour
+    {
+        [SerializeField] public List<GameObject> hearthParent;
+
+
+
+        public void OnDamagePlayer()
+        {
+            hearthParent.RemoveAt(hearthParent.Count);
+        }
+    }
+}
